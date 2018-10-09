ad70876a8009b6d320f7c813fb3106e3f332116c
Destiny Ajax <destinyajakaiye@gmail.com>
2018-10-09 02:39:31 +0100

    Configured project directory structure and application server

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+// const jwt = require('./_helpers/jwt');
+// const errorHandler = require('./_helpers/error_handler');
+
+/** Setting up environment variable **/
+const isProduction = process.env.NODE_ENV === 'production';
+
+const port  = process.env.PORT || 5000;
+const app = express();
+const router = express.Router();
+
+/** set up middlewares */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet())
+
+/** set up routes {API Endpoints} */
+app.use('/api', router)
+app.use('/api', require('./server/routes/user.route'));
+
+// use JWT auth to secure the api
+// app.use(jwt());
+
+// global error handler
+// app.use(errorHandler);
+
+//set server and port
+app.listen(port, () => {
+	console.log('Server running on port ' + port);
+});
